d63a9e6deb71b9d3167cbf433ca7b2e0de5ad2f2
AstolfoPrime <andrewe.smith136@gmail.com>
2024-10-25 11:45:57 -0400

    Finally done

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import pygame as pyg
+=======
+import pygame 
+>>>>>>> 4928fcb (It's done)
 # from sprite import Space_Shuttle
 
 # CONSTANTS
@@ -6,6 +10,7 @@ FPS = 30
 WIDTH = 640
 HEIGHT = 480
 WINDOW_TITLE = "Testing Apollo 13"
+<<<<<<< HEAD
 BACKGROUND_IMAGE = "assets/background_image.png"
 SPACE_SHUTTLE = "assets/space_shuttle.png"
 
@@ -43,10 +48,33 @@ def main() -> None:
     screen.blit(background, (0, 0))
 
     clock = pyg.time.Clock()
+=======
+BACKGROUND_IMAGE = "background_image.png"
+SPACE_SHUTTLE = "space_shuttle.png"
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main() -> None:
+    pygame.init()
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    background =  pygame.image.load(BACKGROUND_IMAGE).convert()
+    
+    space_shuttle = pygame.image.load(SPACE_SHUTTLE)
+    space_shuttle.convert_alpha()
+    space_shuttle = pygame.transform.scale(space_shuttle, (100, 100))
+
+    # Setting up the variables
+    space_shuttle_x = 640
+    space_shuttle_y = 150
+    
+    clock = pygame .time.Clock()
+>>>>>>> 4928fcb (It's done)
 
     running = True
     while running:
         clock.tick(FPS)
+<<<<<<< HEAD
         space_shuttle = Space_Shuttle()
         test = pyg.sprite.Group(space_shuttle)
 
@@ -59,6 +87,24 @@ def main() -> None:
         pyg.display.flip()
 
     pyg.quit()
+=======
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+       
+        # Make it go right
+        space_shuttle_x -= 10
+
+        if space_shuttle_x < 0:
+            space_shuttle_x = 640
+
+        screen.blit(background, (0, 0))
+        screen.blit(space_shuttle, (space_shuttle_x, space_shuttle_y))
+        pygame.display.flip()
+
+    pygame.quit()
+>>>>>>> 4928fcb (It's done)
 
 if __name__ == "__main__":
     main()
